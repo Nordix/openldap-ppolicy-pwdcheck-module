badbffb298cd5e3a14ce0a503ad4fb725b2149f6
Tero Saarni <tero.saarni@est.tech>
2025-09-15 13:26:01 +0300

    Re-format code consistently
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -31,6 +31,21 @@ After running a test, you can find the test output files in the `openldap-src/te
 
 For examples of how to write tests, refer to the [`tests/`](tests/) directory or in the OpenLDAP source code's `openldap-src/tests/scripts` directory.
 
+
+## Code formatting
+
+To format the code, run:
+
+```
+make clang-format
+```
+
+You need to have `clang-format` installed for this to work.
+The formatting style is defined in the [.clang-format](.clang-format) file.
+
+Alternatively you can use the clangd extension in VS Code to format code.
+See the "VS Code tips" section below for instructions on setting up clangd.
+
 ## VS Code tips
 
 For C language support in VS Code, install the [clangd extension](https://marketplace.visualstudio.com/items?itemName=llvm-vs-code-extensions.vscode-clangd) and [`bear`](https://github.com/rizsotto/Bear), which generates a compilation database for the clangd language server.
--- check_password.h
+++ check_password.h
@@ -4,11 +4,11 @@
  * See LICENSE, README and INSTALL files
  */
 
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 #include <portable.h>
 #include <slap.h>
+#include <stdio.h>
+#include <string.h>
 
 #ifdef HAVE_CRACKLIB
 #include <crack.h>
@@ -26,17 +26,16 @@
 #define CONFIG_FILE "/etc/openldap/check_password.conf"
 #endif
 
-#define DEFAULT_QUALITY  3
+#define DEFAULT_QUALITY	 3
 #define DEFAULT_CRACKLIB 1
-#define MEMORY_MARGIN    50
-#define MEM_INIT_SZ      64
-#define FILENAME_MAXLEN  512
+#define MEMORY_MARGIN	 50
+#define MEM_INIT_SZ		 64
+#define FILENAME_MAXLEN	 512
 
-#define PASSWORD_TOO_SHORT_SZ \
-  "Password for dn=\"%s\" is too short (%d/6)"
-#define PASSWORD_QUALITY_SZ \
-  "Password for dn=\"%s\" does not pass required number of strength checks (%d of %d)"
-#define BAD_PASSWORD_SZ \
-  "Bad password for dn=\"%s\" because %s"
+#define PASSWORD_TOO_SHORT_SZ "Password for dn=\"%s\" is too short (%d/6)"
+#define PASSWORD_QUALITY_SZ                                                    \
+	"Password for dn=\"%s\" does not pass required number of strength checks " \
+	"(%d of %d)"
+#define BAD_PASSWORD_SZ "Bad password for dn=\"%s\" because %s"
 
-int check_password (char *pPasswd, char **ppErrStr, Entry *pEntry);
+int check_password(char* pPasswd, char** ppErrStr, Entry* pEntry);
